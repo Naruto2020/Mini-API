7fa64af05172cf85c14e6a35ff4a8c0303d7fac3
Naruto2020 <guyfredy@gmail.com>
2022-07-01 12:42:45 +0200

    set docker-compose file on v1.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
 // server
-app.listen(process.env.PORT, ()=>{
-    console.log(`ecoute sur le port ${process.env.PORT}`);
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log(`ecoute sur le port ${process.env.PORT || 4000}`);
 });
